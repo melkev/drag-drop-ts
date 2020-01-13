4f1c63e2ea6d83fec40691fd894dd6dda6352d3a
melkev <51228075+melkev@users.noreply.github.com>
2020-01-13 12:23:10 +0100

    DOM element interaction

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,42 @@
-
 class ProjectInput {
-  templateElement: HTMLTemplateElement
-  hostElement: HTMLDivElement
-  element: HTMLFormElement
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
-    this.hostElement = document.getElementById('app')! as HTMLDivElement
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true)
-    this.element = importedNode.firstElementChild as HTMLFormElement
-    this.attach()
+    // The importNode() method imports a node from another document.The imported node can be of all node types.
+    const importedNode = document.importNode(this.templateElement.content,true);
+    // The firstElementChild property returns the first child element of the specified element.
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin' , this.element)
+    // The insertAdjacentHTML() method inserts a text as HTML, into a specified position.
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
 
-const prjInput = new ProjectInput()
+const prjInput = new ProjectInput();
