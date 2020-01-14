4b8f9fbde20c37d9cfece9f820b49c0435e89117
melkev <51228075+melkev@users.noreply.github.com>
2020-01-14 19:16:06 +0100

    add : namespaces

--- src/app.ts
+++ src/app.ts
@@ -1,182 +1,19 @@
+/// <reference path="models/drag-drop.ts" />
+/// <reference path="models/project.ts" />
+/// <reference path="state/project-stat.ts" />
+/// <reference path="util/validation.ts" />
+/// <reference path="decorators/autobind.ts" />
 
-//  interface
-interface Validatable {
-  value: string | number
-  required?: boolean
-  minLength?: number
-  maxLength?: number
-  min?: number
-  max?: number
+/// <reference path="components/base-components.ts" />
+/// <reference path="components/project-input.ts" />
+/// <reference path="components/project-item.ts" />
+/// <reference path="components/project-list.ts" />
+
+namespace App {
+
+  new ProjectInput();
+  new ProjectList('active');
+  new ProjectList('finished');
+
 }
 
-
-// function
-// autobind decorator function
-function autoBind(
-  _: any,
-  _2: string,
-  descriptor: PropertyDescriptor
-) {
-  const originalMethod = descriptor.value
-  const adjDescriptor: PropertyDescriptor = {
-    configurable: true,
-    get() {
-      const boundFn = originalMethod.bind(this)
-      return boundFn
-    }
-  }
-  return adjDescriptor
-}
-
-// VALIDATION FUNCTION
-function validate(validatableInput: Validatable) {
-  let isValid = true;
-  if (validatableInput.required) {
-    isValid = isValid && validatableInput.value.toString().trim().length !== 0
-  }
-  if (validatableInput.minLength != null && typeof validatableInput.value === "string") {
-    isValid =
-      isValid && validatableInput.value.length >= validatableInput.minLength
-  }
-  //
-
-  if (validatableInput.maxLength != null && typeof validatableInput.value === "string") {
-    isValid =
-      isValid && validatableInput.value.length <= validatableInput.maxLength
-  }
-  if (validatableInput.min != null && typeof validatableInput.value === "number") {
-    isValid =
-      isValid && validatableInput.value >= validatableInput.min
-  }
-  if (validatableInput.max != null && typeof validatableInput.value === "number") {
-    isValid =
-      isValid && validatableInput.value <= validatableInput.max
-  }
-
-  return isValid
-}
-
-// projectList CLASS
-
-class ProjectList {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLElement;
-
-  constructor(private type: 'active' | 'finished'){
-    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
-
-    // The importNode() method imports a node from another document.The imported node can be of all node types.
-    const importedNode = document.importNode(this.templateElement.content, true);
-    // The firstElementChild property returns the first child element of the specified element.
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.element.id = `${this.type}-projects`;
-
-    this.attach();
-    this.renderContent();
-  }
-
-  private renderContent() {
-    const listId = `${this.type}-projects-list`
-    this.element.querySelector('ul')!.id = listId
-    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS'
-
-
-  }
-
-  private attach() {
-     // The insertAdjacentHTML() method inserts a text as HTML, into a specified position.
-    this.hostElement.insertAdjacentElement('beforeend', this.element);
-  }
-}
-
-// project input claass
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
-  titleInputElement: HTMLInputElement;
-  descriptionInputElement: HTMLInputElement;
-  peopleInputElement: HTMLInputElement;
-
-  constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
-
-    // The importNode() method imports a node from another document.The imported node can be of all node types.
-    const importedNode = document.importNode(this.templateElement.content, true);
-    // The firstElementChild property returns the first child element of the specified element.
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input';
-
-    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
-
-    this.configure();
-    this.attach();
-  }
-
-  private gatherUserInput(): [string, string, number] | void {
-    const enteredTitle = this.titleInputElement.value;
-    const enteredDescription = this.descriptionInputElement.value;
-    const enteredPeople = this.peopleInputElement.value;
-
-    const titleValidatable: Validatable = {
-      value: enteredTitle,
-      required: true
-    }
-    const descriptionValidatable: Validatable = {
-      value: enteredDescription,
-      required: true,
-      minLength: 5
-    }
-    const peopleValidatable: Validatable = {
-      value: +enteredPeople,
-      required: true,
-      min: 1,
-      max:5
-    }
-    if (
-      !validate(titleValidatable) &&
-      !validate(descriptionValidatable) &&
-      !validate(peopleValidatable)
-    ) {
-      alert('invalid input , please try again!');
-      return;
-    } else {
-      return [enteredTitle, enteredDescription, +enteredPeople];
-    }
-  }
-
-  private clearInputs() {
-    this.titleInputElement.value = '';
-    this.descriptionInputElement.value = '';
-    this.peopleInputElement.value = '';
-  }
-
-  @autoBind
-  private submitHandler(event: Event) {
-    event.preventDefault();
-    const userInput = this.gatherUserInput();
-    if (Array.isArray(userInput)) {
-      const [title, desc, people] = userInput;
-      console.log(title, desc, people);
-      this.clearInputs()
-    }
-  }
-
-  private configure() {
-    this.element.addEventListener('submit', this.submitHandler);
-  }
-
-  private attach() {
-    // The insertAdjacentHTML() method inserts a text as HTML, into a specified position.
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
-  }
-}
-
-const prjInput = new ProjectInput();
-const activePrjList = new ProjectList('active');
-const finishedPrjList = new ProjectList('finished');
