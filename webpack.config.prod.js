af958f352145eb76da85764a95e312e10b8ed53f
melkev <51228075+melkev@users.noreply.github.com>
2020-01-15 10:05:05 +0100

    add webpack prod and dev

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,15 @@
 const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode:'development',
+  mode:'production',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname , 'dist'),
-    publicPath: 'dist'
+    path: path.resolve(__dirname , 'dist')
+    
   },
-  devtool: 'inline-source-map',
+  devtool: 'none',
   module: {
     rules: [
       {
@@ -20,5 +21,8 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  pluggins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
 };
